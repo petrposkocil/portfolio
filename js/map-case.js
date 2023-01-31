1775e78d469cd456eb76dea364b190cc388ca7e6
petrposkocil <petr.poskocil@gmail.com>
2023-01-31 23:58:48 +0100

    Case study extension

--- js/map-case.js
+++ js/map-case.js
@@ -1,4 +1,4 @@
-var mapCase = L.map('map-case').setView([50.073658, 14.418540], 11);
+var mapCase = L.map('map-case').setView([50.054658, 14.458540], 11);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -26,38 +26,18 @@ var greyIcon = new L.Icon({
 // var greenIcon = new LeafIcon({iconUrl: 'leaf-green.png'});
 // var greyIcon = new LeafIcon({iconUrl: 'leaf-grey.png'});
 
-var marker = L.marker([52.5200, 13.4050], {icon: greenIcon}).addTo(mapCase).bindPopup("SITE 1"); // site1
-var marker = L.marker([49.1951, 16.6068], {icon: greyIcon}).addTo(mapCase).bindPopup("SITE 2"); // site2
-
-var border = { "type": "Feature", "properties": { "NAME": "KLATOVY" }, "geometry": { "type": "Polygon", "coordinates": [ [ [ 13.28394, 49.39119 ], [ 13.28178, 49.39256 ], [ 13.27894, 49.39225 ], [ 13.27492, 49.39847 ], [ 13.27136, 49.39906 ], [ 13.26764, 49.39967 ], [ 13.266, 49.39994 ], [ 13.26569, 49.40189 ], [ 13.26564, 49.40225 ], [ 13.27542, 49.41175 ], [ 13.27664, 49.41536 ], [ 13.27931, 49.41428 ], [ 13.27806, 49.40564 ], [ 13.27994, 49.40414 ], [ 13.28375, 49.40619 ], [ 13.2865, 49.40675 ], [ 13.29558, 49.40858 ], [ 13.29606, 49.40867 ], [ 13.29831, 49.40483 ], [ 13.30675, 49.4002 ], [ 13.30706, 49.39742 ], [ 13.30403, 49.39578 ], [ 13.30525, 49.38972 ], [ 13.30092, 49.38694 ], [ 13.29894, 49.38567 ], [ 13.29881, 49.38558 ], [ 13.29675, 49.38428 ], [ 13.29747, 49.38297 ], [ 13.30119, 49.37619 ], [ 13.30025, 49.37442 ], [ 13.29969, 49.37439 ], [ 13.29669, 49.37417 ], [ 13.29469, 49.37981 ], [ 13.28989, 49.38055 ], [ 13.28778, 49.38528 ], [ 13.29006, 49.38653 ], [ 13.28822, 49.38847 ], [ 13.28394, 49.39119 ] ] ] } }
+var marker = L.marker([50.030294,14.440091], {icon: greenIcon}).addTo(mapCase).bindPopup("Address: U Krčkého nádraží, 140 00 Praha 4, Czechia"); // site1
 
 
-function getColor(d) {
-    if (d == 'KLATOVY') {
-      return '#800026';
-    } else { return '#FFEDA0';}
-}
-function style(feature) {
-    return {
-        fillColor: getColor(feature.properties.NAME),
-        weight: 2,
-        opacity: 1,
-        color: 'white',
-        dashArray: '3',
-        fillOpacity: 0.7
-    };
-}
 
-L.geoJson(border, {style: style}).addTo(mapCase);
-
-var imageUrl = 'images/case/mcda-map4.png';
+var imageUrl = 'images/case/mcda-map.png';
 var errorOverlayUrl = 'https://cdn-icons-png.flaticon.com/512/110/110686.png';
 var altText = 'MCDA result';
 var latLngBounds = L.latLngBounds([[50.1881128, 14.2137163], [49.912771, 14.7392248]]);
 
 
 var imageOverlay = L.imageOverlay(imageUrl, latLngBounds, {
-    opacity: 0.5,
+    opacity: 1,
     errorOverlayUrl: errorOverlayUrl,
     alt: altText,
     interactive: false
