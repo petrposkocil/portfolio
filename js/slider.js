d7ad97b5a25f987a88f4e4ba0d147b0f3a179d88
Petr Poskocil <petr.poskocil@gmail.com>
2023-10-25 22:39:11 +0200

    Delete js directory

--- js/slider.js
+++
@@ -1,7 +0,0 @@
-$("#slider").on("input change", (e)=>{
-  const sliderPos = e.target.value;
-  // Update the width of the foreground image
-  $('.foreground-img').css('width', `${sliderPos}%`)
-  // Update the position of the slider button
-  $('.slider-button').css('left', `calc(${sliderPos}% - 18px)`)
-});
